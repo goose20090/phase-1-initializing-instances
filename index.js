a9a7f67bae0c9050d94630374550cbe7204ee39b
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2022-10-19 15:07:08 +0100

    commit tree 75c7dc947efbafdab6ba0499d19a4f18f3b01575

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Write your code here
+class Dog{
+    constructor(name, breed){
+        this.name = name;
+        this.breed = breed;
+    }
+}
+
+let bigFluffyDog1 = new Dog("Buzz", "Great Pyrenees");
+let bigFluffyDog2 = new Dog("Woody", "labrador");
+
+
+// Create a class for Breakfast
+// Breakfast will have a constructor with a food and drink
+
+class Breakfast{
+    constructor(food, drink){
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+// Create a class for Lunch
+// Lunch will have a constructor with a salad, a soup and a drink
+
+class Lunch {
+    constructor(salad, soup, drink){
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+
+
+// Create a class for Dinner
+// Dinner will have a constructor with salad, soup entree and dessert
+// Initialise dessert as a private property, with the hash symbol
+
+
+class Dinner{
+    #dessert
+    constructor(salad, soup, entree, dessert){
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this.#dessert = dessert;
+    }
+}
